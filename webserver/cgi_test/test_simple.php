0348b73f55bef598def1c6421f9f79686c979698
fleger42 <florianleger91120@gmail.com>
2022-04-05 20:28:40 +0200

    random

--- webserver/cgi_test/test_simple.php
+++ webserver/cgi_test/test_simple.php
@@ -1,5 +1,8 @@
 <?php
-echo $_ENV['last_name'];
+
+echo getenv('REQUEST_METHOD');
+echo $_POST['REQUEST_METHOD'];
+
 if(isset($_POST['last_name'])) {
     echo "WORK";
     echo $_POST['last_name'];
